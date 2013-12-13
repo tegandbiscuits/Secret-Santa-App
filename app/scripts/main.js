5c665d6de0398a6d55f6ceb49e2f2af2ac310944
Justin Stevens <justinstevens@itracedesigns.com>
2013-12-12 23:05:09 -0600

    Setting up FireBase

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,15 +4,16 @@
   // When the user presses enter on the message input, write the message to firebase.
   $('#fSubmit').on("click",function (e) {
     var name = $('#nameInput').val();
-    var text = $('#messageInput').val();
-    messagesRef.push({name:name, text:text});
-    $('#messageInput').val('');
+    var gift1 = $('#giftInput1').val();
+    var gift2 = $('#giftInput2').val();
+    var gift3 = $('#giftInput3').val();
+    messagesRef.push({name:name, gift1:gift1, gift2:gift2, gift3:gift3});
   });
 
   // Add a callback that is triggered for each chat message.
   messagesRef.limit(10).on('child_added', function (snapshot) {
     var message = snapshot.val();
-    $('<div/>').text(message.text).prepend($('<em/>')
+    $('<div/>').text(message.gift1 +", "+message.gift2 +", "+message.gift3).prepend($('<em/>')
       .text(message.name+': ')).appendTo($('#messagesDiv'));
     $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
   });
