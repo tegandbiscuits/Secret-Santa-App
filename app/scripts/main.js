fd194e1193628fd150d8cf896e04632d15878395
Matthew Nuzum <newz@bearfruit.org>
2013-12-16 18:08:42 -0600

    Fixing JS Lint

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,22 +1,24 @@
-  // Get a reference to the root of the chat data.
-  var messagesRef = new Firebase('https://itracebase.firebaseio.com/chat');
+// Get a reference to the root of the chat data.
+var messagesRef = new Firebase('https://itracebase.firebaseio.com/chat');
 
-  // When the user presses enter on the message input, write the message to firebase.
-  $('#fSubmit').on("click",function (e) {
+// When the user presses enter on the message input, write the message to firebase.
+$('#fSubmit').on('click',function (e) {
+    'use strict';
     var name = $('#nameInput').val();
     var gift1 = $('#giftInput1').val();
     var gift2 = $('#giftInput2').val();
     var gift3 = $('#giftInput3').val();
     messagesRef.push({name:name, gift1:gift1, gift2:gift2, gift3:gift3});
-  });
+});
 
-  // Add a callback that is triggered for each chat message.
-  messagesRef.limit(10).on('child_added', function (snapshot) {
+// Add a callback that is triggered for each chat message.
+messagesRef.limit(10).on('child_added', function (snapshot) {
+    'use strict';
     var message = snapshot.val();
-    $('<div/>').text(message.gift1 +", "+message.gift2 +", "+message.gift3).prepend($('<em/>')
+    $('<div/>').text(message.gift1 +', '+message.gift2 +', '+message.gift3).prepend($('<em/>')
       .text(message.name+': ')).appendTo($('#messagesDiv'));
     $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
-  });
+});
 
 /*
 import json
